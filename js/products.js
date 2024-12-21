7725480447f0bd424c7b06c752891fb7d27467fd
german <germanvelardez16@gmail.com>
2024-12-21 05:52:43 -0300

    Arreglar un par de problemas con formspree

--- js/products.js
+++ js/products.js
@@ -128,18 +128,13 @@ const showBox = (nameBox) => {
         const totalPrice = document.getElementById('total-price');    
         totalPrice.innerHTML = `$${checkTotal()}`;
         let showMeCart = document.getElementById('cart-no-empty');
-        showBox.classList.remove('d-none')
+        showMeCart.classList.remove('d-none')
 
     }
     else{
-        let empty = document.getElementsByClassName("empty-cart")[0];
-        empty.classList.remove('d-none'); // Remueve la clase
-
-
+        let emptyElements = document.getElementsByClassName("empty-cart");
+        emptyElements.forEach(element=>element.classList.remove('d-none')); // Remueve la clase
     }
-
-   
-    
 
 }
 
@@ -150,7 +145,102 @@ console.log(`Creando la canasta de productos: ${myCart}`);
 
 if(window.location.pathname == '/shopcase.html'){
     showBox('myBox');
+
+// Envio de factura
+
+// Recuperar los datos del sessionStorage
+const productos = cart;
+const total = checkTotal();
+
+// Mostrar el resumen de la compra
+const resumen = document.getElementById("detalle");
+let resumenTexto = "";
+
+
+
+
+
+
+resumenTexto += `
+    <table class="table mb-4">
+
+
+        <thead>
+    
+            <tr>
+                <th>Resumen de producto</th>
+                <th>Precio</th>
+            </tr>
+        </thead>
+        <tbody>
+`;
+
+for (let i = 0; i < productos.length; i++) {
+    const producto = productos[i];
+    resumenTexto += `
+        <tr>
+            <td>${producto.name}</td>
+            <td>$${producto.price}</td>
+        </tr>
+    `;
 }
+
+
+resumenTexto += `
+    </tbody>
+    </table>
+`;
+
+
+
+
+
+resumen.innerHTML = resumenTexto;
+
+
+
+
+// Función envío del Formulario
+const  enviarFormulario = (event)=> {
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+
+    // Obtiene datos de contacto
+    const nombre = document.getElementById('nombre').value.trim();
+    const email = document.getElementById('contactoEmail').value.trim();
+    const telefono = document.getElementById('telefono').value.trim();
+
+    if (!nombre  || !email || !telefono) {
+        alert("Por favor, completa todos los campos de contacto.");
+        return; // detiene la ejecucuión de la función hasta que pase el if
+    }
+
+    // Agrega al valor total el signo de pesos $
+    const totalConPesos = `${checkTotal()}`;
+
+    // Asigna los datos a los campos ocultos del formulario
+   document.getElementById('carritoData').value = resumenTexto;
+  document.getElementById('totalCarrito').value = totalConPesos;
+
+    // Envía el formulario a Formspree
+    document.getElementById('formulario').submit();
+}
+
+// Asociamos el evento al botón de enviar
+document.getElementById('botonEnviar').addEventListener('click', enviarFormulario);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
